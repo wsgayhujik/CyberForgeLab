114040d844369a93fa342118765a166df6c664aa
wsgayhujik <gs1jozax123@hotmail.com>
2024-05-05 22:25:53 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("world");
+function permute(nums) {
+  const result = [];
+  backtrack([]);
+  return result;
+  function backtrack(current) {
+    if (current.length === nums.length) {
+      result.push([...current]);
+      return;
+    }
+    for (const num of nums) {
+      if (current.includes(num)) continue;
+      current.push(num);
+      backtrack(current);
+      current.pop();
+    }
+  }
+}
