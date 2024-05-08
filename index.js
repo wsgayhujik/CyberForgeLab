b1a70c3228ee0e4a2c8d272591f0f88b60a14653
wsgayhujik <gs1jozax123@hotmail.com>
2024-05-08 14:14:04 +0700

    commit

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const difference = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => !s.has(x));
-};
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) minIndex = j;
+    }
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
