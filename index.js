4ab94540c5124b6701e2517803a2b32b9284b1f2
wsgayhujik <gs1jozax123@hotmail.com>
2024-05-11 04:19:03 +0700

    demo task commit

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function lengthOfLongestSubstring(s) {
-  const map = new Map();
-  let maxLength = 0;
-  let left = 0;
-  for (let right = 0; right < s.length; right++) {
-    if (map.has(s[right])) {
-      left = Math.max(left, map.get(s[right]) + 1);
-    }
-    map.set(s[right], right);
-    maxLength = Math.max(maxLength, right - left + 1);
-  }
-  return maxLength;
-}
+const drop = (arr, n = 1) => arr.slice(n);
