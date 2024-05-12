4ec3b42af2196278be28386d2107786441c52d96
wsgayhujik <gs1jozax123@hotmail.com>
2024-05-12 21:44:31 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-function maximalSquare(matrix) {
-  if (matrix.length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const dp = Array.from(Array(rows + 1), () => Array(cols + 1).fill(0));
-  let maxSide = 0;
-  for (let i = 1; i <= rows; i++) {
-    for (let j = 1; j <= cols; j++) {
-      if (matrix[i - 1][j - 1] === "1") {
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
-        maxSide = Math.max(maxSide, dp[i][j]);
-      }
-    }
+function rob(nums) {
+  let prevMax = 0;
+  let currMax = 0;
+  for (const num of nums) {
+    const temp = currMax;
+    currMax = Math.max(prevMax + num, currMax);
+    prevMax = temp;
   }
-  return maxSide * maxSide;
+  return currMax;
 }
