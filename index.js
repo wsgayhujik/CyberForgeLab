d5c7d3830b106e95185b3a77306415a8d6c5a7fe
wsgayhujik <gs1jozax123@hotmail.com>
2024-05-13 09:40:22 +0700

    feat

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function rob(nums) {
-  let prevMax = 0;
-  let currMax = 0;
-  for (const num of nums) {
-    const temp = currMax;
-    currMax = Math.max(prevMax + num, currMax);
-    prevMax = temp;
+function lengthOfLIS(nums) {
+  if (nums.length === 0) return 0;
+  const dp = new Array(nums.length).fill(1);
+  let max = 1;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
+    }
+    max = Math.max(max, dp[i]);
   }
-  return currMax;
+  return max;
 }
